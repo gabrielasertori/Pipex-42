0c32263586d1d2240ae68bb87dbc8335e15e74a1
gabrielasertori <gabriela@sertori.com.br>
2022-02-14 23:58:50 +0100

    add files

---
+++ utils/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 16:04:44 by gcosta-d          #+#    #+#             */
+/*   Updated: 2022/02/10 02:20:36 by gcosta-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	j;
+
+	src_len = ft_strlen(src);
+	j = 0;
+	if (size != 0)
+	{
+		while (j < size - 1 && j != src_len)
+		{
+			dest[j] = src[j];
+			j++;
+		}
+		dest[j] = 0;
+	}
+	return (src_len);
+}
