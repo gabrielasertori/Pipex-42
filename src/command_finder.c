17090584eb7048df7cdedcf4b2e367a0b97de0dd
gabrielasertori <gabriela@sertori.com.br>
2022-02-15 02:12:25 +0100

    add is_path_bin function

--- src/command_finder.c
+++ src/command_finder.c
@@ -1,16 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   command_finder_bonus.c                             :+:      :+:    :+:   */
+/*   command_finder.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 18:26:43 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/01/20 16:12:31 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/02/15 02:09:45 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes_bonus/pipex_bonus.h"
+#include "../includes/pipex.h"
+
+static int	is_path_bin(char *command);
 
 char	*command_finder(char *command)
 {
@@ -19,6 +21,8 @@ char	*command_finder(char *command)
 	int		i;
 
 	i = 0;
+	if (is_path_bin(command))
+		return (command);
 	paths = ft_split(VALID_PATHS, ':');
 	while (paths[i])
 	{
@@ -33,4 +37,15 @@ char	*command_finder(char *command)
 	}
 	free_matrix(paths);
 	return (NULL);
+}
+
+static int	is_path_bin(char *command)
+{
+	int	is_path;
+
+	is_path = 0;
+	if (!access(command, F_OK))
+		is_path = 1;
+
+	return (is_path);
 }
